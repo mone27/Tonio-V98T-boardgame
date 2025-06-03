5ff4d157a8465378de0acde51b23c69b5aba348d
APiscitelli <apiscitelli@unibz.it>
2025-06-03 12:02:48 +0200

    Collector (Actual)

--- core/src/main/java/it/unibz/inf/pp/clash/model/snapshot/units/impl/Collector.java
+++ core/src/main/java/it/unibz/inf/pp/clash/model/snapshot/units/impl/Collector.java
@@ -1,4 +1,41 @@
 package it.unibz.inf.pp.clash.model.snapshot.units.impl;
 
-public class Collector {
+import it.unibz.inf.pp.clash.model.snapshot.units.MobileUnit;
+
+public class Collector implements MobileUnit {
+
+    int health;
+    UnitColor color;
+    int attackCountdown;
+
+    public Collector(){
+        this.health = 5;
+        this.color = UnitColor.TWO;
+        this.attackCountdown = 0;
+    }
+
+    @Override
+    public UnitColor getColor() {
+        return this.color;
+    }
+
+    @Override
+    public int getAttackCountdown() {
+        return this.attackCountdown;
+    }
+
+    @Override
+    public void setAttackCountdown(int attackCountDown) {
+        this.attackCountdown = attackCountDown;
+    }
+
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
+
+    @Override
+    public void setHealth(int health) {
+        this.health = health;
+    }
 }
