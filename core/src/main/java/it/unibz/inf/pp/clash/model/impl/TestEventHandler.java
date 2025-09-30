fd12250b7fea670f287222ce9f2e53c630092a04
Tonio-V98T <antonio.piscitelli98@gmail.com>
2025-09-30 19:22:09 +0200

    Test mistakes with git

---
+++ core/src/main/java/it/unibz/inf/pp/clash/model/impl/TestEventHandler.java
@@ -0,0 +1,40 @@
+package it.unibz.inf.pp.clash.model.impl;
+
+import it.unibz.inf.pp.clash.model.EventHandler;
+
+public class TestEventHandler implements EventHandler {
+    @Override
+    public void newGame(String firstHero, String secondHero) {
+
+    }
+
+    @Override
+    public void exitGame() {
+
+    }
+
+    @Override
+    public void skipTurn() {
+
+    }
+
+    @Override
+    public void callReinforcement() {
+
+    }
+
+    @Override
+    public void requestInformation(int rowIndex, int columnIndex) {
+
+    }
+
+    @Override
+    public void selectTile(int rowIndex, int columnIndex) {
+
+    }
+
+    @Override
+    public void deleteUnit(int rowIndex, int columnIndex) {
+
+    }
+}
